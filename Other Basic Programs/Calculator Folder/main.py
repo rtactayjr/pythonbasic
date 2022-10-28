bfb4898c1d58de07ea9a186728061f46fe04b59c
Rolando Tactay <103474360+rtactayjr@users.noreply.github.com>
2022-10-29 03:04:46 +0800

    Update main.py

--- Other Basic Programs/Calculator Folder/main.py
+++ Other Basic Programs/Calculator Folder/main.py
@@ -1,44 +1,55 @@
 from art import logo
 
-
-print(logo)
-print("\nHello! Welcome to my Basic-Python-Calculator. Please enjoy! \n")
-
-
 #create functions for each operator
 
 def addition(n1, n2):
   return n1 + n2
-
   
 def subtraction(n1, n2):
   return n1-n2
 
-  
 def multiplication(n1, n2):
   return n1 * n2
 
-  
 def division(n1, n2):
   return n1 / n2
 
-
-first_number = float(input("Please enter a 1st number: "))
-
+  
+#dictionary of operators
 dict_operations = {
   "+" : addition,
   "-" : subtraction,
   "*" : multiplication,
   "/" : division
 }
-for ops in dict_operations:
-  print("\n"+ ops)
-operators = input("\nChoose an operator: ")
+  
 
-second_number = float(input("\nPlease enter a 2nd number: "))
+def calculator():
+  print(logo)
+  
+  print("\nHello! Welcome to my Basic-Python-Calculator. Please enjoy! \n")
+  
+  first_number = float(input("Please enter a 1st number: "))
 
-calculation_function = dict_operations[operators]
-answer = calculation_function(first_number, second_number)
+  for ops in dict_operations:
+    print("\n"+ ops)
 
-print(f"\nCalculation is = {first_number} {operators} {second_number} = {answer}")
+  should_continue = True
 
+  while should_continue:
+    operators = input("\nChoose an operator: ")
+    
+    second_number = float(input("\nPlease enter a 2nd number: "))
+    
+    calculation_function = dict_operations[operators]
+    answer = calculation_function(first_number, second_number)
+    
+    print(f"\nCalculation is = {first_number} {operators} {second_number} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      first_number = answer
+    else:
+      should_continue = False
+      calculator()
+
+calculator()
