e64ed97a25574d28aa3ee60708b4008536f98c65
Rolando Tactay <103474360+rtactayjr@users.noreply.github.com>
2022-10-29 01:37:39 +0800

    Create art.py

---
+++ Other Basic Programs/Simple Auction program/art.py
@@ -0,0 +1,10 @@
+logo = """
+                                          88                          
+                                    ,d    ""                          
+                                    88                                
+,adPPYYba, 88       88  ,adPPYba, MM88MMM 88  ,adPPYba,  8b,dPPYba,   
+""     `Y8 88       88 a8"     ""   88    88 a8"     "8a 88P'   `"8a  
+,adPPPPP88 88       88 8b           88    88 8b       d8 88       88  
+88,    ,88 "8a,   ,a88 "8a,   ,aa   88,   88 "8a,   ,a8" 88       88  
+`"8bbdP"Y8  `"YbbdP'Y8  `"Ybbd8"'   "Y888 88  `"YbbdP"'  88       88  
+"""
